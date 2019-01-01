4c983120ede7d264a0f4e907ce89142ff61c72a6
Hani Abu Zeid <abuzeid2222@gmail.com>
2019-01-02 00:26:22 +0100

    destructure and more validation and css

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -3,18 +3,28 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 	renderField(field) {
+		const {
+			meta: { touched, error },
+		} = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input className="form-control" type="text" {...field.input} />
-				{field.meta.error}
+				<div className="text-help">{touched ? error : ''}</div>
 			</div>
 		);
 	}
 
+	onSubmit(values) {
+		console.log(values);
+	}
+
 	render() {
+		const { handleSubmit } = this.props;
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field label="Title" name="title" component={this.renderField} />
 				<Field
 					label="Categories"
@@ -26,6 +36,9 @@ class PostsNew extends Component {
 					name="content"
 					component={this.renderField}
 				/>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
 			</form>
 		);
 	}
